b2e055989391105321c5e63ae68f8bee29ba35d9
nievesalvin <alvinn4392@hstat.org>
2018-01-18 16:28:48 +0000

    initialized/go_to_work created

--- oo-final-project.rb
+++ oo-final-project.rb
@@ -1,3 +1,24 @@
-class Blah
+class Building
+    def initialize
+    end
+end
+
+class Tenant
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        @income = 200
+        @bank_account = 100
+        @bills = 0
+    end
     
+    def go_to_work(income)
+        @bank_account = @bank_account + income
+    end
+    
+    def pay_bills(bank_account)
+        
+    end
+    
+    def 
 end
